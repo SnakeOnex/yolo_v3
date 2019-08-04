c9101344402ac273c4d74b8dd886b1c630259732
snakeone <romakcz@gmail.com>
2019-08-04 13:45:15 +0200

    extracting layers from file into pytorch module list

--- darknet.py
+++ darknet.py
@@ -8,6 +8,18 @@ import numpy as np
 import pprint
 pp = pprint.PrettyPrinter()
 
+class EmptyLayer(nn.Module):
+    """
+    OMEGA FUCKING LUL
+    """
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+
 def parse_cfg(cfgfile):
     file = open(cfgfile, 'r')
     lines = file.read().split('\n')
@@ -37,12 +49,116 @@ def create_modules(blocks):
     prev_filters = 3
     output_filters = []
 
+    for index, x in enumerate(blocks[1:]):
+
+        # Get the info about the layer
+        module = nn.Sequential()
+        if (x["type"] == "convolutional"):
+            activation = x["activation"]
+            try:
+                batch_normalize = int(x["batch_normalize"])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+            filters = int(x["filters"])
+            padding = int(x["pad"])
+            kernel_size = int(x["size"])
+            stride = int(x["stride"])
+
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+
+            # Add the convolutional layer
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias)
+            module.add_module("conv_{0}".format(index), conv)
+
+            # Add the Batch Norm layer
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+
+            # Check the activation
+            # It is eitehr Linear or a leaky relu for YOLO
+            if activation == "leaky":
+                activ = nn.LeakyReLU(0.1, inplace = True)
+                module.add_module("leaky_{0}".format(index), activ)
+        # if it's an upsampling layer
+        # use Bilinear2dUpsampling <= no clue what this is
+        elif (x["type"] == "upsample"):
+            stride = int(x["stride"])
+            upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
+            module.add_module("upsample_{}".format(index), upsample)
+
+        # if it is a route layer
+        elif (x["type"] == "route"):
+            x["layers"] = x["layers"].split(',')
+
+            # Start of a route
+            start = int(x["layers"][0])
+
+            # end, if there exists one
+            try:
+                end = int(x["layers"][1])
+            except:
+                end = 0
+
+            # Positive anotation
+            if start > 0:
+                start = start - index
+            if end > 0:
+                end = end - index
+
+            # empty layer OMEGALUL
+            route = EmptyLayer()
+            module.add_module("route_{0}".format(index), route)
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
+            else:
+                filters = output_filters[index + start]
+
+        elif x["type"] == "shortcut":
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{}".format(index), shortcut)
+
+        elif x["type"] == "yolo":
+            mask = x["mask"].split(',')
+            mask = [int(x) for x in mask]
+            
+            anchors = x["anchors"].split(',')
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    return (net_info, module_list)
+
+
+
+
+
+
+
+            
+
 
 blocks = parse_cfg('cfg/yolov3.cfg')
-module_list = nn.ModuleList()
-prev_filters = 3
-output_filters = 3
-print(module_list)
-#pp.pprint(blocks)
+print(create_modules(blocks))
+
+
+
+
+
+
 
 
